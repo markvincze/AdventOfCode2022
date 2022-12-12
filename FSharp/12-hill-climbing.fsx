b33ad7e4db755a784a13361f1f9eab859bfdbc46
Mark Vincze <mrk.vincze@gmail.com>
2022-12-12 23:03:29 +0100

    Day 12 part 2

--- FSharp/12-hill-climbing.fsx
+++ FSharp/12-hill-climbing.fsx
@@ -23,6 +23,9 @@ let endPos = [ for x in 0..Array2D.length1 terrain - 1 do
 terrain.[fst startPos, snd startPos] <- { terrain.[fst startPos, snd startPos] with Elevation = 'a'; BestDist = Some 0; Solved = true }
 terrain.[fst endPos, snd endPos] <- { terrain.[fst endPos, snd endPos] with Elevation = 'z' }
 
+let originalTerrain = Array2D.copy terrain
+originalTerrain.[fst startPos, snd startPos] <- { originalTerrain.[fst startPos, snd startPos] with Elevation = 'a'; BestDist = None; Solved = false }
+
 let queue = [ startPos ]
 
 let accessibleNeighbors (terrain: Square[,]) pos =
@@ -36,24 +39,35 @@ let accessibleNeighbors (terrain: Square[,]) pos =
     }
 
 let rec findShortestPath endPos queue (terrain: Square[,]) =
-    // printfn "Queue: %A" queue
-    let next :: queue = queue |> List.sortBy (fun p -> terrain.[fst p, snd p].BestDist.Value) |> List.distinct
-    let square = terrain.[fst next, snd next]
-    terrain.[fst next, snd next] <- { square with Solved = true }
-    let ns = accessibleNeighbors terrain next
-             |> Seq.filter (fun n -> not n.Solved)
-    // printfn "ns: %A" ns
-    for n in ns do
-        match n.BestDist with
-        | None -> terrain.[fst n.Pos, snd n.Pos] <- { n with BestDist = Some (square.BestDist.Value + 1) }
-        | Some d -> if square.BestDist.Value + 1 < d
-                        then terrain.[fst n.Pos, snd n.Pos] <- { n with BestDist = Some (square.BestDist.Value + 1) }
-    let queue = queue
-                |> List.append (ns |> Seq.map (fun n -> n.Pos) |> List.ofSeq)
-    // printfn "Next queue: %A" queue
-    // printfn "End square: %A" terrain.[fst endPos, snd endPos]
-    if terrain.[fst endPos, snd endPos].Solved
-    then terrain.[fst endPos, snd endPos].BestDist.Value
-    else findShortestPath endPos queue terrain
+    match queue |> List.sortBy (fun p -> terrain.[fst p, snd p].BestDist.Value) |> List.distinct with
+    | [] -> None
+    | next :: queue ->
+        let square = terrain.[fst next, snd next]
+        terrain.[fst next, snd next] <- { square with Solved = true }
+        let ns = accessibleNeighbors terrain next
+                |> Seq.filter (fun n -> not n.Solved)
+        for n in ns do
+            match n.BestDist with
+            | None -> terrain.[fst n.Pos, snd n.Pos] <- { n with BestDist = Some (square.BestDist.Value + 1) }
+            | Some d -> if square.BestDist.Value + 1 < d
+                            then terrain.[fst n.Pos, snd n.Pos] <- { n with BestDist = Some (square.BestDist.Value + 1) }
+        let queue = queue
+                    |> List.append (ns |> Seq.map (fun n -> n.Pos) |> List.ofSeq)
+        if terrain.[fst endPos, snd endPos].Solved
+        then terrain.[fst endPos, snd endPos].BestDist.Value |> Some
+        else findShortestPath endPos queue terrain
 
 let result1 = findShortestPath endPos queue terrain
+
+// Part 2
+let startPositions = [ for x in 0..Array2D.length1 terrain - 1 do
+                           for y in 0..Array2D.length2 terrain - 1 -> (x, y)]
+                     |> List.filter (fun (x, y) -> terrain.[x, y].Elevation = 'a')
+
+let result2 = startPositions
+              |> List.choose
+                (fun sp -> let newTerrain = Array2D.copy originalTerrain
+                           newTerrain.[fst sp, snd sp] <-
+                               { terrain.[fst sp, snd sp] with Elevation = 'a'; BestDist = Some 0; Solved = true }
+                           findShortestPath endPos [ sp ] newTerrain)
+              |> List.min
