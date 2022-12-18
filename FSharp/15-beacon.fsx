ea281c74aae6708be050be3c5d8402578f125b32
Mark Vincze <mrk.vincze@gmail.com>
2022-12-18 15:25:02 +0100

    Day 15 part 2

--- FSharp/15-beacon.fsx
+++ FSharp/15-beacon.fsx
@@ -23,23 +23,28 @@ let sensors = File.ReadAllLines "FSharp/15-beacon-input.txt"
               |> List.ofArray
               |> List.map parse
 
-let blockedRange row sensor =
-    let diff = sensor.Distance - (abs (row - (snd sensor.Position)))
+let blockedRange row limits sensor =
+    let (px, py) = sensor.Position
+    let diff = sensor.Distance - (abs (row - py))
     if diff < 0
     then None
-    else Some ((fst sensor.Position - diff), (fst sensor.Position + diff))
+    else match limits with
+         | None -> Some (px - diff, px + diff)
+         | Some (low, high) -> if px + diff < low || px - diff > high
+                               then None
+                               else Some (max low (px - diff), min high (px + diff))
 
-let blockedRanges sensors row =
+let blockedRanges sensors row limits =
     sensors
-    |> List.choose (blockedRange row)
+    |> List.choose (blockedRange row limits)
 
 let countBlocked blockedRanges =
     let rec countBlocked blockedRanges coveredUntil acc =
         match blockedRanges with
         | [] -> acc
         | (x1, x2) :: t -> match coveredUntil with
-                        | None -> countBlocked t (Some x2) (acc + x2 - x1)
-                        | Some cy -> if x1 > cy then countBlocked t (Some x2) (acc + x2 - x1)
+                           | None -> countBlocked t (Some x2) (acc + x2 - x1 + 1)
+                           | Some cy -> if x1 > cy then countBlocked t (Some x2) (acc + x2 - x1)
                                         else if x2 <= cy then countBlocked t (Some cy) acc
                                         else countBlocked t (Some x2) (acc + x2 - cy)
 
@@ -47,5 +52,17 @@ let countBlocked blockedRanges =
 
 let row = 2000000
 
-let result1 = blockedRanges sensors row
+let result1 = blockedRanges sensors row None
               |> countBlocked
+
+let highLimit = 4000000
+let (missingBeaconRow, _) =
+    [0..highLimit]
+    |> List.map (fun r -> r, blockedRanges sensors r (Some (0, highLimit)) |> countBlocked)
+    |> List.find (fun (r, c) -> c < highLimit)
+
+let blockedRangesInMissingRow = blockedRanges sensors missingBeaconRow (Some (0, highLimit))
+let missingBeaconX = [0..highLimit]
+                     |> List.find (fun x -> List.forall (fun (x1, x2) -> x < x1 || x > x2) blockedRangesInMissingRow)
+
+let result2 = (int64 missingBeaconX) * 4000000L + (int64 missingBeaconRow)
